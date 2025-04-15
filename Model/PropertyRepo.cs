3d111ed0a4e6dcdad889f5a2e13b9c9bd8f05d62
AlizParajuli <A00290934@mycambrian.ca>
2025-04-15 10:08:52 -0400

    Update PropertyRepo.cs

--- Model/PropertyRepo.cs
+++ Model/PropertyRepo.cs
@@ -36,7 +36,7 @@ namespace PropertyApp.Model
             },
             new Property
             {
-                Address = "New Cairo, Cairo",
+                Address = "New Cairo,Cairo",
                 Price = 3500000,
                 DefaultImage= "https://images.pexels.com/photos/1115804/pexels-photo-1115804.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
                 Images = new List<string>
